1eda63008ecab8fff8903003431c5d999e263ada
naol86 <naol.kasinet@a2sv.org>
2024-06-24 23:22:37 +0300

    test

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
 find the correct indexes to paginate the dataset
 '''
 
+
 def index_range(page: int, page_size: int) -> tuple:
     '''return a tuple of size two containing a start index and an end index'''
     return ((page - 1) * page_size, page * page_size)
