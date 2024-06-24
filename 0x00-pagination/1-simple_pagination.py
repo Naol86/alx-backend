74ec75b0c719a2124bffa106187348d555e3379a
naol86 <naol.kasinet@a2sv.org>
2024-06-24 23:31:55 +0300

    test

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -38,6 +38,6 @@ class Server:
         assert isinstance(page_size, int) and page_size > 0
 
         start, end = index_range(page, page_size)
-        if end > len(self.dataset()):
+        if start > len(self.dataset()):
             return []
         return self.dataset()[start:end]
